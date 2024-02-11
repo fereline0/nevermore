ba5b962c9b499db8a82fbfbe9b6bb0938483fc15
fereline0 <shimko.alex.fereline@gmail.com>
2024-02-11 11:51:50 +0300

    code readability

--- src/components/Content/SideBar/Preview/Action/page.tsx
+++ src/components/Content/SideBar/Preview/Action/page.tsx
@@ -1,24 +1,28 @@
-'use client'
+"use client";
 
-import styles from "./page.module.css"
+import styles from "./page.module.css";
 import Button from "@/components/UI/Button/page";
 import ModalWindow from "@/components/ModalWindow/page";
 import { useState } from "react";
 import IAbility from "@/types/ability.type";
 
-export default function Actions(props: IAbility)
-{
-    const [stateVisibility, setStateVisibility] = useState(false);
+export default function Actions(props: IAbility) {
+  const [stateVisibility, setStateVisibility] = useState(false);
 
-    function openWinodw()
-    {
-        setStateVisibility(true);
-    }
+  function openWinodw() {
+    setStateVisibility(true);
+  }
 
-    return (
-        <div className={styles.action}>
-            <Button value={props.name} func={openWinodw} />
-            <ModalWindow title={props.name} description={props.description} func={props.func} getVisibility={stateVisibility} setVisibility={setStateVisibility} />
-        </div>
-    )
+  return (
+    <div className={styles.action}>
+      <Button value={props.name} func={openWinodw} />
+      <ModalWindow
+        title={props.name}
+        description={props.description}
+        func={props.func}
+        getVisibility={stateVisibility}
+        setVisibility={setStateVisibility}
+      />
+    </div>
+  );
 }
--- src/components/Content/SideBar/Preview/page.tsx
+++ src/components/Content/SideBar/Preview/page.tsx
@@ -1,50 +1,68 @@
-'use client'
+"use client";
 
-import { useRouter } from 'next/navigation'
-import { useSession } from "next-auth/react"
-import Avatar from "./Avatar/page"
-import styles from "./page.module.css"
-import IRole from "@/types/role.type"
-import Action from "./Action/page"
-import { deleteUser } from "@/services/user"
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import Avatar from "./Avatar/page";
+import styles from "./page.module.css";
+import IRole from "@/types/role.type";
+import Action from "./Action/page";
+import { deleteUser } from "@/services/user";
 
-interface IPreview
-{
-    id: number,
-    image: string,
-    role: IRole,
+interface IPreview {
+  id: number;
+  image: string;
+  role: IRole;
 }
 
-export default function Preview(props: IPreview)
-{
-    const router = useRouter();
+export default function Preview(props: IPreview) {
+  const router = useRouter();
 
-    const { data: session, status } = useSession();
+  const { data: session, status } = useSession();
 
-    return (
-        <div className={styles.preview}>
-            <Avatar url={props.image} />
-            <div className={styles.role}>
-                <h3 className={styles.role} style={{
-                    backgroundColor: `#${props.role.color}`,
-                }}>{props.role.name}</h3>
-            </div>
-            {props.id != session?.user.id && 
-                status === "authenticated" &&
-                    props.role.id < session?.user.role.id &&
-                    session?.user.role.abilities.map((ability: any) => {
-                        if (ability.slug == "deleteUser") {
-                            return (
-                                <Action name="Delete user" description="You are sure" func={async () => await deleteUser(props.id).then(() => { router.refresh() })} />
-                            )
-                        }
-                        if (ability.slug == "ban") {
-                            return (
-                                <Action name="Ban" description="You are sure" func={async () => await deleteUser(props.id).then(() => { router.refresh() })} />
-                            )
-                        }
-                    })
-            }
-        </div>
-    )
+  return (
+    <div className={styles.preview}>
+      <Avatar url={props.image} />
+      <div className={styles.role}>
+        <h3
+          className={styles.role}
+          style={{
+            backgroundColor: `#${props.role.color}`,
+          }}
+        >
+          {props.role.name}
+        </h3>
+      </div>
+      {props.id != session?.user.id &&
+        status === "authenticated" &&
+        props.role.id < session?.user.role.id &&
+        session?.user.role.abilities.map((ability: any) => {
+          if (ability.slug == "deleteUser") {
+            return (
+              <Action
+                name="Delete user"
+                description="You are sure"
+                func={async () =>
+                  await deleteUser(props.id).then(() => {
+                    router.refresh();
+                  })
+                }
+              />
+            );
+          }
+          if (ability.slug == "ban") {
+            return (
+              <Action
+                name="Ban"
+                description="You are sure"
+                func={async () =>
+                  await deleteUser(props.id).then(() => {
+                    router.refresh();
+                  })
+                }
+              />
+            );
+          }
+        })}
+    </div>
+  );
 }
