9f888afc1c842ba2f096566fa665ca2db01288b9
fereline0 <shimko.alex.fereline@gmail.com>
2024-03-23 13:35:25 +0300

    fixes for next build to work

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["cdn.discordapp.com", "avatars.githubusercontent.com"],
   },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 module.exports = nextConfig;
